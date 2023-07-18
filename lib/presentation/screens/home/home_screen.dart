51460b90bcff6245d7855ce90f39148a2f910c72
Daniel <ccs2024081@est.univalle.edu>
2023-07-18 17:50:51 -0400

    home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -7,8 +8,51 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
-        centerTitle: true,
+        centerTitle: false,
       ),
+      body: const _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Mostar un texto con el nombre de la primera opction del menu
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      // * Builder -> se construira en tiempo de ejecucion
+      itemBuilder: (context, index){
+          final menuItem = appMenuItems[index];
+           return _CustomListTitle(menuItem: menuItem);
+      }
+
+    );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+         leading: Icon(menuItem.icon, color: colors.primary), //Para poner el icono
+         trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+         title: Text(menuItem.title),
+         subtitle: Text(menuItem.subTitle),
+         // * El onTap para que redireccione a otras pantallas
+         onTap: () {
+           // TODO: navegar a otras pantallas
+         },
+     );
   }
 }
